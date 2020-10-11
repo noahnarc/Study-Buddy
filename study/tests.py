cf9ed71426beff74e564cfbf8a2ced97f712f30f
klive614 <44390496+klive614@users.noreply.github.com>
2020-10-10 22:29:40 -0400

    Update tests.py
    I used the example test case to try to get travis to build successfully. Will add better test for google login later.

--- study/tests.py
+++ study/tests.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 # Create your tests here.
 
 # dummy tests from textbook exchange example to get travis to build successfully
-
 class DummyTestCase(TestCase):
   def setUp(self):
     x = 1
