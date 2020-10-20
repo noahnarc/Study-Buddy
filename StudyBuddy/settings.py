f63ae038ddc111aded529bcc329d2ae535b3cb3c
Sarah Snow <ses9nd@virginia.edu>
2020-10-20 17:11:31 -0400

    Fixing except statement

--- StudyBuddy/settings.py
+++ StudyBuddy/settings.py
@@ -11,15 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from Exception import ImportError
 
 # Try to import django-heroku depending on Travis or Heroku
 try:
     # Configure Django App for Heroku.
     import django_heroku
     django_heroku.settings(locals())
-except ImportError:
-    found = False
+except:
+    None
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
