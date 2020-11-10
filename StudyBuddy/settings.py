ea6e893fe99af95b8e047d0eabcc6b1377998946
Caroline Chen <cc8tt@virginia.edu>
2020-11-10 00:01:21 -0500

    added profile tags, tags redirect to group search page

--- StudyBuddy/settings.py
+++ StudyBuddy/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
-import os
+#import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,8 +130,8 @@ DATABASES = {
     # SQLite
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+        'NAME': BASE_DIR / 'db.sqlite3',
+        #'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 
     # PostgreSQL
