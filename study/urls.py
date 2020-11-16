5a19b348edfb7d6e2965dc402e5f2acab9a7d791
Sarah Snow <ses9nd@virginia.edu>
2020-11-16 14:27:35 -0500

    Login required and template updates

--- study/urls.py
+++ study/urls.py
@@ -3,13 +3,15 @@ URLs
 """
 
 from django.urls import path
+from django.contrib.auth.decorators import login_required
+
 from . import views
 
 app_name = 'study'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('profile', views.update_profile, name='profile'),
-    path('search', views.SearchResultsView.as_view(), name='search'),
-    path('groups/<int:pk>', views.GroupView.as_view(), name='group'),
-    path('create-group', views.CreateGroup.as_view(), name='create-group'),
+    path('search', login_required(views.SearchResultsView.as_view()), name='search'),
+    path('groups/<int:pk>', login_required(views.GroupView.as_view()), name='group'),
+    path('create-group', login_required(views.CreateGroup.as_view()), name='create-group'),
 ]
